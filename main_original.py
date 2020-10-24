6995080b9cbec802c58e613277dca92c19c97391
thomas rochais <thb@sas.upenn.edu>
2020-10-24 16:53:30 -0400

    Cleaned up the names of a few files

--- main_original.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from game import Game
-#from game_original import Game
-
-if __name__ == "__main__":
-    g = Game()
-    while g.running:
-        g.curr_menu.display_menu()
-        g.game_loop()
