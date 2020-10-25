5dfba1b8fe25ffc4c94052ac5c5e49a4088f3df2
thomas rochais <thb@sas.upenn.edu>
2020-10-24 22:20:48 -0400

    yapf made a bunch of formatting changes

--- game.py
+++ game.py
@@ -1,32 +1,34 @@
 import pygame
-from pygame import key
 from menu import *
-from rocket import *
+from rocket import Rocket
+
+
 class Game():
     def __init__(self):
         pygame.init()
         self.running, self.playing = True, False
-        self.LEFT_KEY, self.RIGHT_KEY= False, False
-        self.UP_KEY, self.DOWN_KEY= False, False
+        self.LEFT_KEY, self.RIGHT_KEY = False, False
+        self.UP_KEY, self.DOWN_KEY = False, False
         self.START_KEY, self.BACK_KEY = False, False
         self.DISPLAY_W, self.DISPLAY_H = 600, 800
-        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
-        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
+        self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
+        self.window = pygame.display.set_mode(
+            ((self.DISPLAY_W, self.DISPLAY_H)))
         self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
         self.main_menu = MainMenu(self)
         self.options = OptionsMenu(self)
         self.credits = CreditsMenu(self)
         self.curr_menu = self.main_menu
-        self.rocket=Rocket(self)
+        self.rocket = Rocket(self)
 
     def game_loop(self):
         while self.playing:
-            pygame.key.set_repeat(10,5) #So rocket can keep moving when pressing and holding keys
-            self.display.fill(self.BLACK) #Black screen
+            pygame.key.set_repeat(10, 5)  # So rocket can keep moving when pressing and holding keys
+            self.display.fill(self.BLACK)  # Black screen
             self.check_events()
             if self.START_KEY:
-                self.playing= False
+                self.playing = False
             if self.LEFT_KEY:
                 self.rocket.move_left()
             if self.RIGHT_KEY:
@@ -34,7 +36,7 @@ class Game():
             if self.UP_KEY:
                 self.rocket.move_up()
             if self.DOWN_KEY:
-                self.rocket.move_down() 
+                self.rocket.move_down()
             self.redrawGameWindow()
             self.reset_keys()
 
@@ -56,20 +58,20 @@ class Game():
                     self.LEFT_KEY = True
                 if event.key == pygame.K_RIGHT:
                     self.RIGHT_KEY = True
-                    
+
     def reset_keys(self):
         self.LEFT_KEY, self.RIGHT_KEY = False, False
         self.UP_KEY, self.DOWN_KEY = False, False
         self.START_KEY, self.BACK_KEY = False, False
 
-    def draw_text(self, text, size, x, y ):
-        font = pygame.font.Font(self.font_name,size)
+    def draw_text(self, text, size, x, y):
+        font = pygame.font.Font(self.font_name, size)
         text_surface = font.render(text, True, self.WHITE)
         text_rect = text_surface.get_rect()
-        text_rect.center = (x,y)
-        self.display.blit(text_surface,text_rect)
+        text_rect.center = (x, y)
+        self.display.blit(text_surface, text_rect)
 
     def redrawGameWindow(self):
-        self.rocket.blit_rocket() #Draw the rocket
-        self.window.blit(self.display, (0,0)) #Blitting is drawing
+        self.rocket.blit_rocket()  # Draw the rocket
+        self.window.blit(self.display, (0, 0))  # Blitting is drawing
         pygame.display.update()
--- rocket.py
+++ rocket.py
@@ -1,29 +1,29 @@
 import pygame
 import os 
+
+
 class Rocket():
     def __init__(self, game):
-        self.game = game #Use a game object
-        #self.width = self.game.DISPLAY_W / 20
-        #self.height = self.game.DISPLAY_H / 20
-        #Initial position of the rectangle (bottom, centered)
+        self.game = game  # Use a game object
+        # Initial position of the rectangle (bottom, centered)
         self.spaceship = pygame.image.load(os.path.join("assets/sprites", "spaceship-a.svg"))
-        self.width, self.height= self.spaceship.get_size()
-        #Rescale spaceship
-        self.width, self.height = round(self.width*.4), round(self.height*.4)
-        self.spaceship = pygame.transform.scale(self.spaceship, (self.width,self.height))
-        #Initial position and velocity
-        self.x, self.y = self.game.DISPLAY_W / 2 - self.width/2, self.game.DISPLAY_H - self.height
-        self.vel=1 #Moving velocity
+        self.width, self.height = self.spaceship.get_size()
+        # Rescale spaceship
+        self.width, self.height = round(self.width * .4), round(self.height * .4)
+        self.spaceship = pygame.transform.scale(self.spaceship, (self.width, self.height))
+        # Initial position and velocity
+        self.x, self.y = self.game.DISPLAY_W / 2 - self.width / 2, self.game.DISPLAY_H - self.height
+        self.vel = 1  # Moving velocity
 
     def blit_rocket(self):
-        self.game.display.blit(self.spaceship, (self.x,self.y))
+        self.game.display.blit(self.spaceship, (self.x, self.y))
 
     def move_left(self):
         if self.x - self.vel > 0:
             self.x -= self.vel
 
     def move_right(self):
-        if self.x + self.vel < self.game.DISPLAY_W - self.width: 
+        if self.x + self.vel < self.game.DISPLAY_W - self.width:
             self.x += self.vel
 
     def move_up(self):
