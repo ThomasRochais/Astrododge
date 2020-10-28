b9114e70d8002dcd21ea1a8fffa63e21e73bd77c
thomas rochais <thb@sas.upenn.edu>
2020-10-27 22:04:55 -0400

    reformated some comments

--- projectiles.py
+++ projectiles.py
@@ -6,11 +6,11 @@ class Projectile():
     def __init__(self, rocket):
         self.rocket = rocket
         self.game = rocket.game
-        self.bullet = pygame.image.load(os.path.join("assets/sprites", "asteroid2.png")) #  Change
+        self.bullet = pygame.image.load(os.path.join("assets/sprites", "asteroid2.png"))  # Change
         self.width, self.height = self.spaceship.get_size()
         self.x, self.y = self.rocket.x + self.rocket.width, self.rocket.y + self.rocket.height / 2
-        self.vel = self.rocket.vel * 3 #  Adjust velocity
-        self.freq = self.width // self.vel * 3 #  Adjust frequency of bullets per frame
+        self.vel = self.rocket.vel * 3  # Adjust velocity
+        self.freq = self.width // self.vel * 3  # Adjust frequency of bullets per frame
 
     def projectiles_loop(self):
         pass
