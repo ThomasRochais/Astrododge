2939d5a37ce33773a2342f3a17ef1517f739884b
Thomas Rochais <thb@sas.upenn.edu>
2021-06-19 00:54:19 -0400

    Added sprites
    Lint errors
    
    Another lint error
    
    Lint error

--- projectile.py
+++ projectile.py
@@ -6,7 +6,7 @@ class Projectile():
     def __init__(self, rocket):
         self.rocket = rocket
         self.game = rocket.game
-        self.image = pygame.image.load(os.path.join("assets/sprites", "bullet2.png")) 
+        self.image = pygame.image.load(os.path.join("assets/sprites", "bullet2.png"))
         self.image = pygame.transform.rotate(self.image, -90)
         self.width, self.height = self.image.get_size()
         # self.width, self.height = round(self.width * .05), round(self.height * .05)
